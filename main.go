ffdad3c5e7c647ef897fdbf9d7b568bdd9be9ead
DeWalle <ashiruhabeeb47@gmail.com>
2023-09-08 08:54:42 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Slack_Name		string		`json:"slack_name"`
+	Current_Day		string		`json:"current_day"`
+	UTC_Time		time.Time	`json:"utc_time"`
+	Track			string		`json:"track"`
+	Github_File_URL	string		`json:"github_file_url"`
+	Github_Repo_URL	string		`json:"github_repo_url"`
+	Status_Code		int			`json:"status_code"`
+
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/hng/internship/first-task/:slack_name/:track", func(ctx *gin.Context) {
+		slack_name := ctx.Param("slack_name")
+		track := ctx.Param("track")
+
+		res := &Response{
+			Slack_Name:      slack_name,
+			Current_Day:     time.Now().Weekday().String(),
+			UTC_Time:        time.Now().UTC(),
+			Track:           track,
+			Github_File_URL: "https://github.com/ashiruhabeeb/hng-internship/blob/main/main.go",
+			Github_Repo_URL: "https://github.com/ashiruhabeeb/hng-internship",
+			Status_Code:     200,
+		}
+
+		ctx.JSON(http.StatusOK, res)
+	})
+
+	router.Run(":8090")
+}
